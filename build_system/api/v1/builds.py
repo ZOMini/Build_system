9d2a3a1b342102fdf2da08da0f6978953db5d377
ZOMini <zarikacr@gmail.com>
2023-05-26 18:17:27 +0300

    v1.4(aiofile_c+)

--- build_system/api/v1/builds.py
+++ build_system/api/v1/builds.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 
-from api.v1 import response
+from api.v1.response import exception_404, response_404
 from models.build_model import BodyBuildModel, TasksResponse
 from services.api_service import APIService, get_api_service
 from services.data_service import full_data
@@ -8,12 +8,12 @@ from services.data_service import full_data
 router = APIRouter()
 
 
-@router.post('/get_tasks', responses=response.RESPONSE_404)
+@router.post('/get_tasks', responses = response_404)  # type: ignore
 async def get_tasks(
     body: BodyBuildModel,
     builds_service: APIService = Depends(get_api_service)
 ) -> TasksResponse:
     if body.build not in full_data.keys():
-        raise HTTPException(*response.EXCEPTION_404)
+        raise HTTPException(*exception_404)
     resp = await builds_service.get_tasks_by_build(body)
-    return TasksResponse(__root__ =resp)
+    return TasksResponse(__root__ = resp)
--- build_system/api/v1/response.py
+++ build_system/api/v1/response.py
@@ -2,5 +2,6 @@ from http import HTTPStatus
 
 from models.build_model import Response_404
 
-RESPONSE_404 = {HTTPStatus.NOT_FOUND: {'model': Response_404}}
-EXCEPTION_404 = (HTTPStatus.NOT_FOUND, 'No this build.')
+response_404 = {HTTPStatus.NOT_FOUND: {'model': Response_404}}
+exception_404 = (HTTPStatus.NOT_FOUND, 'No this build.')
+
